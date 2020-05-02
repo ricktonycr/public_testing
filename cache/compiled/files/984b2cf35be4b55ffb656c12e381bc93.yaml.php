f3e58b025be2e261fca5eaa0b88396910635da83
Richard Antony Cruz Rojas <ricktonycr@gmail.com>
2020-05-02 17:40:17 -0500

    fisrt

---
+++ cache/compiled/files/984b2cf35be4b55ffb656c12e381bc93.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'plugins://git-sync/git-sync.yaml',
+    'modified' => 1588458030,
+    'data' => [
+        'enabled' => true
+    ]
+];
